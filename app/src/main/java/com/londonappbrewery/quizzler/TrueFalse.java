fda662be97bee7d6ea1f78c1e9b4903ab6f2233b
BHSEvan <emwitthun@barabooschools.net>
2017-12-20 08:25:40 -0600

    First

---
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -0,0 +1,34 @@
+package com.londonappbrewery.quizzler;
+
+/**
+ * Created by emwitthun on 12/14/17.
+ */
+
+public class TrueFalse {
+
+    private int mQuestionID;
+    private boolean mAnswer;
+
+    public TrueFalse(int mQuestionID, boolean mAnswer) {
+        this.mQuestionID = mQuestionID;
+        this.mAnswer = mAnswer;
+
+
+    }
+
+    public int getmQuestionID() {
+        return mQuestionID;
+    }
+
+    public void setmQuestionID(int mQuestionID) {
+        this.mQuestionID = mQuestionID;
+    }
+
+    public boolean ismAnswer() {
+        return mAnswer;
+    }
+
+    public void setmAnswer(boolean mAnswer) {
+        this.mAnswer = mAnswer;
+    }
+}
